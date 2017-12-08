510cd850ea81e2a6e4a0c8c2e34ba56296b63805
viinay <viinay@users.noreply.github.com>
2017-12-08 10:55:55 +0530

    Create encoding.js

---
+++ encoding.js
@@ -0,0 +1,11 @@
+/*
+  
+*/
+
+'use strict';
+
+let data = 'stackabuse.com';  
+let buff = new Buffer(data);  
+let base64data = buff.toString('base64');
+
+console.log('"' + data + '" converted to Base64 is "' + base64data + '"');  
